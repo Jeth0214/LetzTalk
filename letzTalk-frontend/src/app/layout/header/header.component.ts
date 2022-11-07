d6b4f0603f0c0fefe12afa65cd4e439dd2fc8db9
Roland Jethro Suyom <rolandjethrosuyom@yahoo.com>
2022-11-07 06:56:08 +0300

    started user-profile feature

--- letzTalk-frontend/src/app/layout/header/header.component.ts
+++ letzTalk-frontend/src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  closeResult = '';
 
-  constructor() { }
+  constructor(
+    private offCanvasService: NgbOffcanvas
+  ) { }
 
   ngOnInit(): void {
   }
 
+  showUserProfile(content: any) {
+    if (this.offCanvasService.hasOpenOffcanvas()) {
+      return;
+    }
+
+    this.offCanvasService.open(content, { ariaLabelledBy: 'user-profile', position: 'end', backdrop: false }).result.then(
+      (result) => {
+        console.log(result)
+      },
+      (reason) => {
+        console.log(reason)
+      },
+    );
+  }
 }
